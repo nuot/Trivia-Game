96683b9d74aa4e7526aafb54e469e2cf6fdfc33b
Nuo <nuot@uci.edu>
2019-03-25 14:33:33 -0700

    add more js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ const quizContent = [
 
     },
     q2 = {
-        question: "what color is it?"
+        question: "what color is it?",
         choices: {
             a:"white", 
             b:"black", 
@@ -19,7 +19,8 @@ const quizContent = [
             d:"golden"
         },
         correctAnswer: "c"
-    }
+    },
+
     q3 = {
         question: "what is my cat's name?",
         choices: {
@@ -30,7 +31,7 @@ const quizContent = [
         },
         correctAnswer: 'a'
 
-    }
+    },
 
     q4 = {
         question: "what is my cat's name?",
@@ -45,7 +46,7 @@ const quizContent = [
 
     },
     q5 = {
-        question: "what color is it?"
+        question: "what color is it?",
         choices: {
             a: "lucky", 
             b: "cutey", 
@@ -54,7 +55,7 @@ const quizContent = [
         },
         correctAnswer: 'a'
 
-    }
+    },
     q6 = {
         question: "what is my cat's name?",
         choices: {
@@ -65,7 +66,7 @@ const quizContent = [
         },
         correctAnswer: 'a'
 
-    }
+    },
 
 q7 = {
         question: "what is my cat's name?",
@@ -80,7 +81,7 @@ q7 = {
 
     },
     q8 = {
-        question: "what color is it?"
+        question: "what color is it?",
         choices: {
             a: "lucky", 
             b: "cutey", 
@@ -89,7 +90,7 @@ q7 = {
         },
         correctAnswer: 'a'
 
-    }
+    },
     q9 = {
         question: "what is my cat's name?",
         choices: {
@@ -103,7 +104,7 @@ q7 = {
 
     },
     q10 ={
-        question:"what color is it?"
+        question:"what color is it?",
         choices: {
             a: "lucky", 
             b: "cutey", 
@@ -114,22 +115,51 @@ q7 = {
 
     }
 ]
+console.log(quizContent[0].choices)
 
 let timer
-let correct
-let inCorrect
+let correct = 0
+let inCorrect = 0
+let currQues = 0
+let i = 0
 
-const init = _ =>{
-    timer = 10
-    document.querySelector('#timer').innerHTML = '10s'
+const dispQuestion = _ =>{
+    if (i >= quizContent.length){
+        document.querySelector('#quiz').innerHTML = `<h1>Game over!</h1>`
+        document.querySelector('#answer').innerHTML = `<h1>Your score:</h1>`
+        document.querySelector('#timer').innerHTML = `<h1>Try again</h1>`
+
+    } else {
+        timer = 20
+        quiz = quizContent[i].question
+        choice = quizContent[i].choices
+        rightAnswer = quizContent[i].correctAnswer
+        document.querySelector('#quiz').innerHTML = '<h3>' + quiz + '</h3>'
+        document.querySelector('#choices').innerHTML = '<p> a: </p>'+ quizContent[0].choices.a +'<p> b: </p>'+quizContent[0].choices.b
 }
-
-const restart = _ =>{
-    document.querySelector(`#quiz`).innerHTML = ''
-    document.querySelector(`#answer`).innerHTML= ''
-    
 }
+dispQuestion()
+
+// const init = _ =>{
+//     timer = 20
+//     document.querySelector('#timer').innerHTML = `Time Remining : ${timer} seconds`
+// }
+
+// const timeLeft = _ =>{
+//     document.querySelector('#timer').innerHTML =  `Time Remining : ${timer} seconds`
+//     timer --
+// }
+
+// const stopper = _ =>{
+//     clearInterval()
+// }
+
+// const restart = _ =>{
+//     document.querySelector(`#quiz`).innerHTML = ''
+//     document.querySelector(`#answer`).innerHTML= ''
+// }
 
 
-init()
 
+
+// init()
